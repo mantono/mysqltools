98219df7ff7ae0c1cebd6937edbcc789aedeed87
Anton Österberg <anton.osterberg@gmail.com>
2018-09-20 23:20:36 +0200

    Updated version. Updated dependencies. Added atomic versionfor SQL queries.

--- src/main/kotlin/com/mantono/mysqltools/Database.kt
+++ src/main/kotlin/com/mantono/mysqltools/Database.kt
@@ -4,12 +4,10 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import java.sql.Connection
 
-object Database
-{
+object Database {
 	private val dataSource: HikariDataSource
 
-	init
-	{
+	init {
 		val config = HikariConfig("database.properties")
 		if(config.jdbcUrl.isNullOrEmpty())
 			config.jdbcUrl = System.getenv("JDBC_URL") ?: throw IllegalArgumentException("No valid JDBC URL found")
@@ -20,8 +18,21 @@ object Database
 	fun connection(): Connection = dataSource.connection
 }
 
-inline fun <T> query(sqlFunc: Connection.() -> T): T
-{
-	val connection = Database.connection()
-	connection.use{ return sqlFunc(connection) }
+inline fun <T> query(
+		connection: Connection = Database.connection(),
+		sqlFunc: Connection.() -> T
+): T {
+	connection.use { return sqlFunc(connection) }
+}
+
+inline fun <T> atomicQuery(
+		connection: Connection = Database.connection(),
+		transactionLevel: Int = Connection.TRANSACTION_REPEATABLE_READ,
+		sqlFunc: Connection.() -> T
+): T {
+	connection.autoCommit = false
+	connection.transactionIsolation = transactionLevel
+	return query(connection) { sqlFunc(connection) }.also {
+		connection.commit()
+	}
 }
